9042995556e6830c3add025bbe84597c66222566
Laksh <laksh@genei.io>
2020-01-10 14:52:42 +0000

    adding apex

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "hello world"
-#pip install -r requirements.txt
+echo "Installing Apex for 16-bit training (this makes training much faster)"
+pip install -r requirements.txt
 
-#git clone https://github.com/NVIDIA/apex
-#pip install -v --no-cache-dir --global_option="--cpp_ext" --global-option="--cuda_ext" ./apex
+git clone https://github.com/NVIDIA/apex
+pip install -v --no-cache-dir --global_option="--cpp_ext" --global-option="--cuda_ext" ./apex
 
